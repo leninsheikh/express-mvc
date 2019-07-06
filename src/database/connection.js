5dfef3e60860e88c6db1313a508f58082040447a
leninsheikh <justtlenin@gmail.com>
2019-07-06 19:38:00 +0600

    edit config file convention

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,4 @@
 import Sequelize from 'sequelize';
-import config from '@src/config/database';
+import config from '@src/config/database.config';
 
 export default new Sequelize(config.database, config.username, config.password, config);
