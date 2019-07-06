d133778c0f9b4ecaddd1fbd7f2f24830e57893fb
leninsheikh <justtlenin@gmail.com>
2019-07-06 20:13:34 +0600

    fixed database config for sequelize cli

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,7 +1,13 @@
-export default {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABSE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_TYPE
+
+if (process.env.ENV === 'development')  {
+    require('dotenv').config();
+}
+module.exports = {
+    development: {
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABSE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
+        dialect: process.env.DATABASE_TYPE
+    }
 }
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
-import config from '@src/config/database.config';
+import configs from '@src/config/database.config';
+const env = process.env.ENV || 'development';
 
+const config = configs[env];
 export default new Sequelize(config.database, config.username, config.password, config);
