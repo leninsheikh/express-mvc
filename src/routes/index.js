7152607049e7a87aa59166d2b24051c07e3ce110
leninsheikh <justtlenin@gmail.com>
2019-07-15 21:48:38 +0600

    testing mail with node mailer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const Express = require('express');
 const passport = require('passport');
+const NodeMailer  = require('nodemailer');
 
 const  admin = require('./web/admin.route');
 const auth = require('./api/auth.route');
@@ -12,7 +13,32 @@ const user = require('./api/user.route');
 const router = Express.Router();
 router.use('/auth', auth);
 router.use('/admin', admin);
-
+router.get('/mail', (req, res) => {
+    let transporter = NodeMailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        secure: false,
+        auth: {
+            // should be replaced with real sender's account
+            user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD
+        }
+    });
+    let mailOptions = {
+        // should be replaced with real recipient's account
+        to: 'ddr7@getnada.com',
+        subject: "EXPRESS NodeMailer",
+        body: "Simple Text",
+        from: process.env.MAIL_FROM_ADDRESS
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info);
+    });
+    res.send('ok');
+});
 /*
  * passport authenticate middleware
  */
